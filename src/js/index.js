32bcb1ba63bdc59bf6113b220d349a17cb77f951
Victor Ocumura <ocumuravictor@gmail.com>
2024-06-09 02:28:01 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,52 @@
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+let cartaoAtual = 0;
+const cartoes = document.querySelectorAll(".cartao");
+
+
+btnAvancar.addEventListener("click", function(){
+
+    
+    if (cartaoAtual === cartoes.length - 1) return;  
+    console.log(cartoes.length);  
+    
+    
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+
+
+
+ 
+
+    cartaoAtual++;
+    
+    cartoes[cartaoAtual].classList.add("selecionado");
+
+
+
+});
+
+
+btnVoltar.addEventListener("click", function(){
+
+    console.log(cartaoAtual);  
+    if (cartaoAtual === 0) return;  
+   
+    
+    
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+
+
+
+ 
+
+    cartaoAtual--;
+    
+    cartoes[cartaoAtual].classList.add("selecionado");
+
+
+
+});
+
+
